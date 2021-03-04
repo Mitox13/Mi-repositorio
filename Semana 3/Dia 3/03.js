04070c925957bf40cf585bc6499c1cd7ba51ec3e
Jaime <ramoslaurente13@gmail.com>
2021-03-03 22:16:57 -0500

    empezando js

---
+++ Semana 3/Dia 3/03.js
@@ -0,0 +1,13 @@
+/**
+ * pagina 44: Ejercicio 2.6
+ */
+let catetoA = +prompt("a");
+let catetoB = +prompt("b");
+console.log(catetoA);
+console.log(catetoB);
+
+
+let hipotenusa;
+
+hipotenusa = (catetoA ** 2 + catetoB ** 2) ** 0.5
+console.log(`La hipotenusa es = ${hipotenusa}`);
